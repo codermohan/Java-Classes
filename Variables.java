049bf939ca648b45007e6b999502d0907bdd8372
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-07-03 14:07:06 -0500

    Added new files

--- Variables.java
+++ Variables.java
@@ -1,5 +1,14 @@
 public class Variables {
     public static void main(String[] args){
-        
+        int noOfLikes = 0;
+        int noOfComments =0;
+        float pricePerKg = 200.50f;
+        char singleChar = 'D';
+        boolean isLiked= true;
+
+        boolean disLiked = false;
+        System.out.println(noOfLikes);
+        System.out.println(isLiked);
+
     }
 }
