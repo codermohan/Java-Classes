60877303d2bed7e68780c9fd73dfeafa210bebaa
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-07-11 14:47:55 -0500

    Added new files

---
+++ Loops.java
@@ -0,0 +1,19 @@
+class Loops{
+    public static void main(String[] args){
+        String[] phoneList = { "Iphone 16",
+        "Iphone 16 Pro max",
+        "Iphone 16 Mini", 
+        "IPhone"
+    };
+
+for(int i=0; i<phoneList.length; i++){
+    System.out.println(phoneList[i]);
+}
+int count =0;
+while (count < 5){
+    System.out.println("Count is :"+count);
+    count++;
+}
+
+    }
+}
