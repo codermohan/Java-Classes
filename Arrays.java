2ed41a46cad6a4360232a48b62cab9a25f0eebd2
Mohan Venkata Krishna Namburi <mohankrishnan802@gmail.com>
2025-07-11 10:55:29 -0500

    Added new files

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+class Arrays {
+    public static void main(String[] args){
+        String prod1Title = "Apple Iphone 16 Promax";
+        String prod2Title = "Apple Iphone 16 Pro";
+        String prod3Title = "Apple Iphone 16 Mini";
+
+        String[] titles = {"Apple Iphone 16 Promax", "Apple Iphone 16 Pro", "Apple Iphone 16 Mini"};
+        
+        String[] titles2 = {
+    "Apple Iphone 16 Promax",
+    "Apple Iphone 16 Pro",
+    "Apple Iphone 16 Mini"
+};
+        System.out.println(titles[2]);
+        System.out.println(titles2.length);
+    }
+}
